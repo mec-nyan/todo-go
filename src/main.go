0f29c76cb289591e865d4cddfe4d3729b7e42b54
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-04-24 20:15:15 -0300

    First version: Load notes.

--- src/main.go
+++ src/main.go
@@ -1,32 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	tea "charm.land/bubbletea/v2"
 )
 
-type model int
+type Note struct {
+	Summary string `json:"summary"`
+	Items   []Note `json:"items"`
+}
+
+type Data struct {
+	Notes []Note `josn:"notes"`
+}
+
+func loadNotes(filename string) (*Data, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load file %s: %w", filename, err)
+	}
+
+	var data Data
+	if err := json.Unmarshal(content, &data); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON: %w", err)
+	}
+
+	return &data, nil
+}
+
+type model struct {
+	Data
+	Current  int
+	Collapse bool
+	Quit     bool
+}
+
+type fileLoader struct {
+	Data  *Data
+	Error error
+}
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return func() tea.Msg {
+		// TODO: Use configuration or command line arguments for the file path.
+		data, err := loadNotes("data.json")
+		return fileLoader{
+			Data:  data,
+			Error: err,
+		}
+	}
+
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
+	case fileLoader:
+		// TODO: Handle errors properly.
+		if msg.Error != nil {
+			return m, tea.Quit
+		}
+
+		m.Data = *msg.Data
+		return m, nil
+
 	case tea.KeyPressMsg:
 
 		switch msg.Code {
 
-		case 'j', '+':
-			m++
+		case 'j':
+			m.Current++
+			if m.Current == len(m.Notes) {
+				m.Current = 0
+			}
 
-		case 'k', '-':
-			m--
+		case 'k':
+			m.Current--
+			if m.Current < 0 {
+				m.Current = len(m.Notes) - 1
+			}
+
+		case 'c':
+			m.Collapse = true
+
+		case 'o':
+			m.Collapse = false
 
 		case 'q', tea.KeyEscape:
+			m.Quit = true
 			return m, tea.Quit
 		}
 	}
@@ -34,16 +100,39 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() tea.View {
-	out := "Counter App\n\n"
-	out += fmt.Sprintf("    Count: %d\n\n", m)
-	out += "\x1b[2mj/+: increment; k/-: decrement; q/esc: quit\x1b[0m\n"
-	return tea.NewView(out)
+
+	if m.Quit {
+		return tea.NewView("Bye!")
+	}
+
+	var view strings.Builder
+
+	view.WriteString("Notes:\n\n")
+
+	for i, note := range m.Notes {
+		cursor := " "
+		if i == m.Current {
+			cursor = ">"
+		}
+
+		more := " "
+		if len(note.Items) > 0 {
+			more = "+"
+		}
+
+		fmt.Fprintf(&view, "  %s %s %s\n", cursor, more, note.Summary)
+	}
+
+	// TODO: There are bubbles for this.
+	view.WriteString("\n\n\x1b[2m  j : down - k : up - q : quit\n")
+
+	return tea.NewView(view.String())
 }
 
 func main() {
-	var m model = 0
 
-	app := tea.NewProgram(m)
+	app := tea.NewProgram(model{Collapse: true})
+
 	if _, err := app.Run(); err != nil {
 		log.Fatalf("Ooooooooooops! (%v)", err)
 	}
