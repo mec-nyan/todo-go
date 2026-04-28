835d5558a907bc8982b9415fd172786a74bbea46
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-04-28 20:24:09 -0300

    Add graphics settings.

---
+++ graphics.go
@@ -0,0 +1,57 @@
+package main
+
+// Our application can run in one of four graphic modes:
+// - ASCII
+// - Unicode (default)
+// - Iconic
+// - Emoji
+//
+// For each, we'll use/allow a different set of characters.
+// This should be transparent and the rest of our app doesn't even need to know about it.
+// I hope...
+
+// Here's a simple example showing what I want to do.
+// It's implementation will certainly change as we add more stuff.
+// Maybe some features will only be available if we're in certain graphics mode.
+// (i.e. we can have an "add emoji" dialog only if emoji is supported, etc).
+
+// TODO: Rename this or `Glyph` (see conifg.go).
+// Glyphs will hold the allowed glyphs.
+type Glyphs struct {
+	// TODO: strings or runes?
+	Cursor string
+	More   string
+	Less   string
+	Bullet string
+}
+
+func GetGlyphs(mode Glyph) Glyphs {
+	// TODO: Select appropriate symbols for each set.
+	glyphs := Glyphs{
+		// TODO: Consider removing the extra space at the end.
+		// (It's there for full-width symbols to overflow).
+		// (It also aligns better with full-width characters).
+		Cursor: "> ",
+		More:   "+ ",
+		Less:   "- ",
+		Bullet: "* ",
+	}
+
+	switch mode {
+	case ASCII:
+		return glyphs
+
+	case Unicode:
+		glyphs.Cursor = "🠊 "
+		glyphs.More = "＋"
+		glyphs.Less = "－"
+		glyphs.Bullet = " •"
+
+	// TODO: Set the glyphs for each set.  BTW since Iconic and Emoji both imply the use of
+	// Unicode, we can safely skip this for now.
+	case Iconic:
+	case Emoji:
+	}
+
+	return glyphs
+}
